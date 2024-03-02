1b7e678ccccebd6e3332dc57da25c7f2f51f1797
Gourav Patel <gp1611gp@gmail.com>
2024-03-02 15:12:16 +0530

    connect to mongo atlas

---
+++ src/db/conn.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+
+
+const connectDB  = async () =>{
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log("Mongo Db connected succsessfull")
+    }catch(err){
+        console.log(err)
+        process.exit(1)
+    }
+}
+
+
+export default connectDB ;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import dotenv from "dotenv"
+import connectDB from "./db/conn.js"
+
+
+dotenv.config({
+    path: '.env'
+})
+
+
+
+connectDB();
